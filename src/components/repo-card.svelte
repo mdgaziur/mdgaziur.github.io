73746c655e750844b7615535ebb5d6560402378a
mdgaziur <mdgaziurrahmannoor@gmail.com>
2023-06-17 23:10:08 +0600

    Contrast and design: redesign parts of the website to improve contrast

--- src/components/repo-card.svelte
+++ src/components/repo-card.svelte
@@ -14,7 +14,7 @@
 	});
 </script>
 
-<div class="repo-card flex flex-col g-5">
+<div class="md:w-[400px] w-full repo-card flex flex-col g-5">
 	<div class="sm:p-10 p-5 flex-1">
 		<h1 class="sm:text-2xl text-sm whitespace-pre-wrap">{repo}</h1>
 		<p class="sm:text-md text-sm font-semibold">{description}</p>
@@ -26,7 +26,7 @@
 				width="24"
 				height="24"
 				viewBox="0 0 24 24"
-				style="fill: #ffffff"
+				style="fill: #cecece"
 				><path
 					d="m6.516 14.323-1.49 6.452a.998.998 0 0 0 1.529 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 0 0-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 0 0-1.822 0L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.214 4.107zm2.853-4.326a.998.998 0 0 0 .832-.586L12 5.43l1.799 3.981a.998.998 0 0 0 .832.586l3.972.315-3.271 2.944c-.284.256-.397.65-.293 1.018l1.253 4.385-3.736-2.491a.995.995 0 0 0-1.109 0l-3.904 2.603 1.05-4.546a1 1 0 0 0-.276-.94l-3.038-2.962 4.09-.326z"
 				/></svg
@@ -39,7 +39,7 @@
 				width="24"
 				height="24"
 				viewBox="0 0 24 24"
-				style="fill: #ffffff"
+				style="fill: #cecece"
 				><path
 					d="M8.465 11.293c1.133-1.133 3.109-1.133 4.242 0l.707.707 1.414-1.414-.707-.707c-.943-.944-2.199-1.465-3.535-1.465s-2.592.521-3.535 1.465L4.929 12a5.008 5.008 0 0 0 0 7.071 4.983 4.983 0 0 0 3.535 1.462A4.982 4.982 0 0 0 12 19.071l.707-.707-1.414-1.414-.707.707a3.007 3.007 0 0 1-4.243 0 3.005 3.005 0 0 1 0-4.243l2.122-2.121z"
 				/><path
@@ -53,23 +53,17 @@
 
 <style lang="scss">
 	.repo-card {
-		width: 400px;
-		border: 1px solid #ffffff;
-		mix-blend-mode: color-dodge;
-
-		h1 {
-			color: #ffffff;
-		}
+		border: 1px solid #cecece;
 
 		p {
-			color: #f6f6f6;
+			color: #e1e1e1;
 		}
 
 		a {
-			border: 1px solid #ffffff;
+			border: 1px solid #cecece;
 			border-bottom: none;
 			border-right: none;
-			color: #ffffff;
+			color: #cecece;
 			font-weight: 700;
 
 			&:nth-child(1) {
