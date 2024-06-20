6e7a61f092a6e71ee666100c9804fdd632e09eda
mdgaziur <mdgaziurrahmannoor@gmail.com>
2024-06-21 02:40:34 +0600

    New: add section for listing skills

---
+++ src/components/skill_card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<div
+	class="skill-card shadow-black/[0.25] shadow-lg flex items-center w-full max-w-md sm:gap-5 gap-3 border-[1px] sm:p-10 p-5 bg-white/[0.02] rounded-xl border-white/[0.11] hover:bg-white/5 transition-colors"
+>
+	<slot />
+	<h2 class="text-2xl">{name}</h2>
+</div>
