2c2b33a6546dc700f8ee2d7006172a2575b56f7d
mdgaziur <mdgaziurrahmannoor@gmail.com>
2025-05-05 14:39:09 +0600

    Codebase: cleanup code

--- src/components/achievement_card.svelte
+++ src/components/achievement_card.svelte
@@ -11,10 +11,10 @@
 	<div class="flex flex-col gap-1">
 		<p class="sm:text-xl text-md flex gap-2">
 			<svg
-				width="25"
+				fill="none"
 				height="30"
 				viewBox="0 0 36 37"
-				fill="none"
+				width="25"
 				xmlns="http://www.w3.org/2000/svg"
 			>
 				<path
@@ -26,14 +26,17 @@
 		</p>
 		<p class="sm:text-xl text-md flex gap-2 items-center">
 			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="25"
 				height="25"
-				viewBox="0 0 24 24"
 				style="fill: #AFD6F6;"
-				><path
+				viewBox="0 0 24 24"
+				width="25"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
 					d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"
-				></path><path d="M13 7h-2v5.414l3.293 3.293 1.414-1.414L13 11.586z"></path></svg
+				></path>
+				<path d="M13 7h-2v5.414l3.293 3.293 1.414-1.414L13 11.586z"></path>
+			</svg
 			>
 			{year}
 		</p>
--- src/components/education_card.svelte
+++ src/components/education_card.svelte
@@ -12,10 +12,10 @@
 	<div class="flex flex-col gap-1">
 		<p class="text-xl flex gap-2">
 			<svg
-				width="25"
+				fill="none"
 				height="30"
 				viewBox="0 0 48 49"
-				fill="none"
+				width="25"
 				xmlns="http://www.w3.org/2000/svg"
 			>
 				<path
@@ -31,16 +31,20 @@
 		</p>
 		<p class="text-xl flex gap-2 items-center">
 			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="25"
 				height="25"
-				viewBox="0 0 24 24"
 				style="fill: #AFD6F6;"
-				><path
-					d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"
-				></path><path d="M13 7h-2v5.414l3.293 3.293 1.414-1.414L13 11.586z"></path></svg
+				viewBox="0 0 24 24"
+				width="25"
+				xmlns="http://www.w3.org/2000/svg"
 			>
-			{start} {#if end} - {end} {/if}
+				<path
+					d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"
+				></path>
+				<path d="M13 7h-2v5.414l3.293 3.293 1.414-1.414L13 11.586z"></path>
+			</svg
+			>
+			{start}
+			{#if end} - {end} {/if}
 		</p>
 	</div>
 </div>
